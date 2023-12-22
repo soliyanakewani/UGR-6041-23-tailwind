267d6cd28e50077bd7bb5a75b1502a2120bb8ead
soliyanakewani <soliyanakewani228@gmail.com>
2023-12-23 00:01:56 +0300

    Tailwind

---
+++ src/js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('burger').addEventListener('click', function() {
+            var navLinks = document.getElementById('navLinks');
+            navLinks.classList.toggle('hidden');
+        });
